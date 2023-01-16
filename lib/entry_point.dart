d2a59ebda68546313d51f64598fb6ed311493d9b
melniknikolay <unclenikoboss@gmail.com>
2023-01-16 13:52:42 +0300

    first commit

---
+++ lib/entry_point.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+import 'components/btm_nav_item.dart';
+import 'model/menu.dart';
+import 'utils/rive_utils.dart';
+
+class EntryPoint extends StatefulWidget {
+  const EntryPoint({super.key});
+
+  @override
+  State<EntryPoint> createState() => _EntryPointState();
+}
+
+class _EntryPointState extends State<EntryPoint>
+    with SingleTickerProviderStateMixin {
+  bool isSideBarOpen = false;
+
+  Menu selectedBottonNav = bottomNavItems.first;
+
+  late SMIBool isMenuOpenInput;
+
+  void updateSelectedBtmNav(Menu menu) {
+    if (selectedBottonNav != menu) {
+      setState(() {
+        selectedBottonNav = menu;
+      });
+    }
+  }
+
+  late AnimationController _animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200))
+      ..addListener(
+            () {
+          setState(() {});
+        },
+      );
+    animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+        parent: _animationController, curve: Curves.fastOutSlowIn));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Transform.translate(
+        offset: Offset(0, 100 * animation.value),
+        child: SafeArea(
+          child: Container(
+            padding:
+            const EdgeInsets.only(left: 12, top: 12, right: 12, bottom: 12),
+            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+            decoration: BoxDecoration(
+              color: Colors.black38.withOpacity(0.8),
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38.withOpacity(0.3),
+                  offset: const Offset(0, 20),
+                  blurRadius: 20,
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ...List.generate(
+                  bottomNavItems.length,
+                      (index) {
+                    Menu navBar = bottomNavItems[index];
+                    return BtmNavItem(
+                      navBar: navBar,
+                      press: () {
+                        RiveUtils.changeSMIBoolState(navBar.rive.status!);
+                        updateSelectedBtmNav(navBar);
+                      },
+                      riveOnInit: (artboard) {
+                        navBar.rive.status = RiveUtils.getRiveInput(artboard,
+                            stateMachineName: navBar.rive.stateMachineName);
+                      },
+                      selectedNav: selectedBottonNav,
+                    );
+                  },
+                ),
+              ],
+            ),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
